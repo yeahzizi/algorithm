113ee6c3164f9010e78e9f7750020d407313b315
yeahzizi <newyeahzizi@gmail.com>
2022-08-05 01:56:25 +0900

    swea 2056

--- 2056. 연월일 달력.py
+++ 2056. 연월일 달력.py
@@ -1,5 +1,25 @@
 T = int(input())
 
 for test_case in range(1, T + 1):
-    n = int(input())
-    total = 0
+    n = input()
+    thirty_one = ("01", "03", "05", "07", "08", "10", "12")
+    thirty = ("04", "06", "09", "11")
+
+    if (n[4:6] == "02") and (1 <= int(n[6:]) <= 28):
+        print ('#%d'%test_case, f'{n[:4]}/{n[4:6]}/{n[6:]}')
+    elif (n[4:6] in thirty_one) and (1 <= int(n[6:]) <= 31):
+        print ('#%d'%test_case, f'{n[:4]}/{n[4:6]}/{n[6:]}')
+    elif (n[4:6] in thirty) and (1 <= int(n[6:]) <= 30):
+        print ('#%d'%test_case, f'{n[:4]}/{n[4:6]}/{n[6:]}')
+    else:
+        print('#%d'%test_case, -1)
+    
+        
+
+    
+        
+
+
+     
+
+
