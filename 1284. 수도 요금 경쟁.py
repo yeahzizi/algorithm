4986ace4137286aaea85214ce73fd2bdd1932692
yeahzizi <newyeahzizi@gmail.com>
2022-08-07 02:37:24 +0900

    SWEA 1284

--- 1284. 수도 요금 경쟁.py
+++ 1284. 수도 요금 경쟁.py
@@ -1,4 +1,11 @@
-#sys.stdin = open("input.txt", "r")
 T = int(input())
 for test_case in range(1, T + 1):
+    P, Q, R, S, W = map(int, input().split())
+
+
+    if (W <= R):
+        print('#%d'%test_case, min(Q, P*W))
+    else:
+        print('#%d'%test_case, min(P*W, (Q + (((W-R)*S)))))
+
     
