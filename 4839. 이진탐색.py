eb1988e5af8eb52c5fbed0aed0bfed648617a791
yeahzizi <newyeahzizi@gmail.com>
2022-08-12 22:37:33 +0900

    swea 4839

--- 4839. 이진탐색.py
+++ 4839. 이진탐색.py
@@ -11,11 +11,12 @@ for test_case in range(1, T + 1):
                 maxIdx = j
         arr[maxIdx], arr[i] = arr[i], arr[maxIdx]  # 최소값을 구간 맨 앞으로
 
-    for n in range(0, N, 2):
+    for n in range(0, 10, 2):
         cnt[n] = arr[int(n/2)]
         cnt[n+1] = arr[int(-(n/2)-1)]
 
-    print(cnt)
+    print(f'#{test_case}', end=' ')
+    print(*cnt)
 
 
 
