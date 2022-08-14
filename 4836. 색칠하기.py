49fc4ea8332c74af782e7a72dbeb9663b96803db
yeahzizi <newyeahzizi@gmail.com>
2022-08-14 23:49:11 +0900

    swea 4836

---
+++ 4836. 색칠하기.py
@@ -0,0 +1,19 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+    space = [[0] * 10 for _ in range(10)]
+    for i in range(1, N+1):
+        r1, c1, r2, c2, color = list(map(int, input().split()))
+
+        count = 0
+        for r in range(r1, r2+1):
+            for c in range(c1, c2+1):
+                space[r][c] += color
+
+
+    for r in range(10):
+        for c in range(10):
+            if space[r][c] >= 3:
+                count += 1
+
+    print(f'#{test_case} {count}')
