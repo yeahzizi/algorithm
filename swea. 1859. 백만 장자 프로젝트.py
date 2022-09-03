1cf48551803667f21a91e1eab29fbcbd6045e4fc
yeahzizi <newyeahzizi@gmail.com>
2022-09-03 10:30:47 +0900

    sweq 1859

--- swea. 1859. 백만 장자 프로젝트.py
+++ swea. 1859. 백만 장자 프로젝트.py
@@ -5,10 +5,18 @@ T = int(input())
 for tc in range(1, 11):
     N = int(input())
     num = list(map(int, input().split()))
+    profit = 0
+    end = num[- 1]
 
-    for i in range(N-1, -1, -1):
-        end = num[N-1] #마지막 값을 갱신하기
-        if
+    for i in range(N-2, -1, -1):
+        if num[i] > end:
+            end = num[i] #마지막 값으로 갱신(마지막 값은 항상 구매하지 않으므로)
+
+        else:
+            profit += end - num[i]
+
+    print(f'#{tc} {profit}')
+
 
 
 
