e4e3db055545994cb8e6bd4a8814177db18338bc
yeahzizi <newyeahzizi@gmail.com>
2022-09-06 23:09:31 +0900

    swea 1984

--- swea. 1984. 중간 평균값 구하기.py
+++ swea. 1984. 중간 평균값 구하기.py
@@ -1,3 +1,10 @@
 T = int(input())
 for tc in range(1, T+1):
-    num = list(map(int, input().split())) 
+    num = list(map(int, input().split()))
+
+    num.remove(max(num))
+    num.remove(min(num))
+
+    avg = round(sum(num) / len(num))
+
+    print(f'#{tc} {avg}')
