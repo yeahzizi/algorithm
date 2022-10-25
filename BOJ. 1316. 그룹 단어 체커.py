af80a132d75767274fc0686ea9fe329358426aff
yeahzizi <newyeahzizi@gmail.com>
2022-10-25 23:57:23 +0900

    boj 1316 푸는중

--- BOJ. 1316. 그룹 단어 체커.py
+++ BOJ. 1316. 그룹 단어 체커.py
@@ -1,28 +1,22 @@
 N = int(input())
 alp = [input() for i in range(N)]
-ans = [] * N
-
-cnt = N
+alp_dict = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10,
+            'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19,
+            'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25}
+cnt = [0] *26
+ans = []
 
 for i in range(N):
-    for j in alp[i]:
-        ans.append(j)
+    for j in range(len(alp[i])):
+        ans.append(alp[i][j])
     ans.append(1)
 
-for i in range(len(ans)-2):
-    if ans[i] == ans[i+1]:
-        ans.remove(ans[i])
-
-result = []
 for i in ans:
-    if i in result:
-        cnt -= 1
-        result.append(i)
-    elif i not in result:
-        result.append(i)
-    elif i == 1:
-        result = []
+    for key, value in alp_dict:
+        if i == key:
+            cnt[value] += 1
 
 
-print(cnt)
+
+
 
