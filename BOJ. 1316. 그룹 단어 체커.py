aeee6a19419353550ac40c2f68c6215b71cbd612
yeahzizi <newyeahzizi@gmail.com>
2022-10-26 23:48:17 +0900

    BOJ 1316 품

--- BOJ. 1316. 그룹 단어 체커.py
+++ BOJ. 1316. 그룹 단어 체커.py
@@ -1,22 +1,13 @@
 N = int(input())
-alp = [input() for i in range(N)]
-alp_dict = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10,
-            'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19,
-            'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25}
-cnt = [0] *26
-ans = []
+cnt = N
 
-for i in range(N):
-    for j in range(len(alp[i])):
-        ans.append(alp[i][j])
-    ans.append(1)
+for _ in range(N):
+    word = input()
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            continue
+        elif word[i] in word[i+1:]:
+            cnt -= 1
+            break
 
-for i in ans:
-    for key, value in alp_dict:
-        if i == key:
-            cnt[value] += 1
-
-
-
-
-
+print(cnt)
