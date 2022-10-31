e5c8517a32fc3712b1f2dbec9ab795e61e0e1ab7
yeahzizi <newyeahzizi@gmail.com>
2022-10-31 23:31:30 +0900

    BOJ 2644

---
+++ BOJ. 2644. 촌수계산.py
@@ -0,0 +1,26 @@
+n = int(input())
+a, b = map(int, input().split())
+matrix = [[0] * (n+1) for _ in range(n+1)]
+m = int(input())
+for m in range(m):
+    x, y = map(int, input().split())
+    matrix[x][y] = 1
+    matrix[y][x] = 1
+
+stack = [a]
+visited = []
+
+while stack:
+    current = stack.pop()
+    if current not in visited:
+        visited.append(current)
+    for destination in range(n+1):
+        if matrix[current][destination] and destination not in visited:
+            stack.append(destination)
+
+print(visited)
+if b not in visited:
+    print(-1)
+else:
+    pass
+    #촌수
