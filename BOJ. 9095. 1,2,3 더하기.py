f0f9963146dd8b8a349a9176f102795ada35fcba
yeahzizi <newyeahzizi@gmail.com>
2022-12-05 23:54:10 +0900

    boj 9095

--- BOJ. 9095. 1,2,3 더하기.py
+++ BOJ. 9095. 1,2,3 더하기.py
@@ -1,6 +1,13 @@
 T = int(input())
+dp = [0] * 11     #11보다 작은 수
 for _ in range(T):
-    dp = [0] * 12
     n = int(input())
-    for i in range(n):
-        dp[i] += 1
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, 11):
+        dp[i] = dp[i-1] + dp[i-2] +dp[i-3]
+
+    print(dp[n])
