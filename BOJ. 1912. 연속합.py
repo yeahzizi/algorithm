dfb132221065397ef8fa04108e09013bf08975a5
yeahzizi <newyeahzizi@gmail.com>
2022-12-09 23:55:46 +0900

    boj 연속합 푸는중

---
+++ BOJ. 1912. 연속합.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr = list(map(int, input().split()))
+dp = [0] * N
+dp[0] = arr[0]
+
+for i in range(1, N):
+    dp[i] = max(dp[i], dp[i-1] + dp[i])
+    print(dp)
+print(max(dp))
