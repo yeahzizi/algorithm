e21b99ba5abf6a39c8b33362c8b1529720c90fa7
yeahzizi <newyeahzizi@gmail.com>
2022-12-16 02:16:35 +0900

    BOJ 1463 주석 추가

--- BOJ. 1463. 1로 만들기.py
+++ BOJ. 1463. 1로 만들기.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 N = int(input())
 dp = [0] * (N+1)
 
+#바텀업 방식
 for i in range(2, N+1):
     dp[i] = dp[i-1] + 1
     # i값이 2, 3으로 나누어지지 않아서 1로 빼야하는 값
@@ -12,7 +13,6 @@ for i in range(2, N+1):
         dp[i] = min(dp[i], dp[i//2]+1)
     #i값이 2로 나누어 떨어지면,
     #위에서 1을 뺀 값이랑, 2로 나눠준 값에서 1을 더한 값 중에 작은 값을 택한다.
-    #바텀업 방식
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i//3]+1)
 
