0175c004bfb64ee72aafd47b872a05d2c1dddff6
yeahzizi <newyeahzizi@gmail.com>
2022-12-19 23:58:11 +0900

    BOJ 1427

--- BOJ. 1427. 소트인사이드.py
+++ BOJ. 1427. 소트인사이드.py
@@ -3,9 +3,11 @@ num = []
 
 for i in range(len(N)):
     num.append(int(N[i]))
+    #입력받은 값을 쪼개서 정수로 리스트에 저장
 
-num.sort(reverse=True)
+num.sort(reverse=True) #저장한 리스트를 큰 수부터 정렬
 for i in num:
     print(i, end='')
+    #리스트에 저장 된 수를 세로가 아닌 가로로 붙여서 출력
 
 
