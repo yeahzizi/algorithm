21a962ffd9e4c29c1b2d3b4407548f48760713a4
yeahzizi <newyeahzizi@gmail.com>
2022-12-28 04:21:22 +0900

    BOJ 1260

--- BOJ. 1260. DFS와 BFS.py
+++ BOJ. 1260. DFS와 BFS.py
@@ -1,6 +1,41 @@
 N, M, V = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-visited = []
+graph = [[0] * (N+1) for _ in range(N+1)]
 
 for i in range(M):
+    n, m = map(int, input().split())
+    graph[n][m] += 1
+    graph[m][n] += 1
+
+dfs_visited = [0] * (N+1)
+
+def dfs(V):
+    dfs_visited[V] = 1
+    print(V, end=' ')
+    for i in range(N+1):
+        if dfs_visited[i] == 0 and graph[i][V]:
+            dfs(i)
+
+dfs(V)
+
+from collections import deque
+
+bfs_visited = [0] * (N+1)
+def bfs(V):
+    bfs_visited[V] = 1
+    queue = deque()
+    queue.append(V)
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+        for i in range(N+1):
+            if bfs_visited[i] == 0 and graph[i][node]:
+                queue.append(i)
+                bfs_visited[i] = 1
+
+
+print()
+bfs(V)
+
+
+
 
