6b9e1ace5b4fd76375c049c88961599502d9465c
yeahzizi <newyeahzizi@gmail.com>
2023-01-01 18:39:20 +0900

    BOJ 14501

--- BOJ. 14501. 퇴사.py
+++ BOJ. 14501. 퇴사.py
@@ -1,8 +1,17 @@
 N = int(input())
-consult = []
+T = []
+P = []
+dp = [0] * (N+1)
 
-for i in range(N):
-    n, m = map(int, input().split())
-    consult.append((n, m))
+for _ in range(N):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
 
-print(consult)
+for i in range(N-1, -1, -1):
+    if i + T[i] > N:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(P[i] + dp[T[i] + i], dp[i+1])
+
+print(max(dp))
