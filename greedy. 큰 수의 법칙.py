a49e69c9c197101427f7eb84ab143e9cfdabe3cd
yeahzizi <newyeahzizi@gmail.com>
2023-06-17 19:38:32 +0900

    이것이 코딩테스트다

--- greedy. 큰 수의 법칙.py
+++ greedy. 큰 수의 법칙.py
@@ -1,3 +1,6 @@
+# 5 8 3
+# 2 4 5 4 6
+
 n, m, k = map(int, input().split())
 num = list(map(int, input().split()))
 
@@ -5,14 +8,19 @@ num.sort()
 first = num[n-1]
 second = num[n-2]
 
-count = m // (k+1) * k
-print(count)
+ans = 0
 
-count += m % (k+1)
-print(count)
+for i in range(m+1):
+    if i == 0:
+        ans += 0
+    elif i == 1:
+        ans += first
+    elif i == 2:
+        ans += first
+    elif i % 3 == 1:
+        ans += second
+    else:
+        ans += first
 
-result = 0
-result += (count) * first
-result += (m - count) * second
+print(ans)
 
-print(result)
