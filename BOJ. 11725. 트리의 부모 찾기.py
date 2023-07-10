d7b59a25e8bfd81a5018aeaadba230a7f4559d69
yeahzizi <newyeahzizi@gmail.com>
2023-07-10 16:56:09 +0900

    dfs

--- BOJ. 11725. 트리의 부모 찾기.py
+++ BOJ. 11725. 트리의 부모 찾기.py
@@ -1,20 +1,26 @@
-N = int(input())
-arr = [[0] * (N+1) for _ in range(N+1)]
-for i in range(N-1):
-    n, m = map(int, input().split())
-    arr[n][m] += 1
-    arr[m][n] += 1
+import sys
+sys.setrecursionlimit(10**6)
 
-visited = []
-stack = [2]
+N = int(sys.stdin.readline())
 
-while stack:
-    current = stack.pop()
-    if current not in visited:
-        visited.append(current)
+tree = [[] for _ in range(N+1)]
+for _ in range(N - 1):
+    n, m = map(int, sys.stdin.readline().split())
+    tree[n].append(m)
+    tree[m].append(n)
 
-    for destination in range(N-1):
-        if destination not in visited and arr[current][destination]:
-            stack.append(destination)
+visited = [False] * (N+1)
+parent = [0 for _ in range(N+1)]
 
-print(visited)
+def dfs(node):
+    visited[node] = True
+    for i in tree[node]:
+        if not visited[i]:
+            parent[i] = node
+            dfs(i)
+    return
+
+dfs(1)
+for i in range(2, len(parent)):
+    print(parent[i])
+
