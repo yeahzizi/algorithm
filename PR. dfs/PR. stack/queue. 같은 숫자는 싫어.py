328601203f591f0b3ee4e17e43b60afc40dc7d70
yeahzizi <newyeahzizi@gmail.com>
2023-07-26 00:28:43 +0900

    PR. level1

---
+++ PR. dfs/PR. stack/queue. 같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+
+    for i in arr:
+        if len(answer) == 0:
+            answer.append(i)
+        elif len(answer) > 0 and answer[-1] != i:
+            answer.append(i)
+
+    return answer
