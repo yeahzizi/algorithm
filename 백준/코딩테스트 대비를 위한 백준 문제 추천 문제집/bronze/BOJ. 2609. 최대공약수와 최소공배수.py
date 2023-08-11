e5b176942adee80d916a0e7cf9f81bf63f3a005b
yeahzizi <newyeahzizi@gmail.com>
2023-08-11 14:34:44 +0900

    함정주의

---
+++ 백준/코딩테스트 대비를 위한 백준 문제 추천 문제집/bronze/BOJ. 2609. 최대공약수와 최소공배수.py
@@ -0,0 +1,15 @@
+# n, m 둘 중의 작은 수 하나가 최대공약수가 될 수 있다는 사실을 잊지말기
+
+n, m = map(int, input().split())
+
+divide = []
+for i in range(1, min(n, m) + 1):
+    if n % i == 0 and m % i == 0:
+        divide.append(i)
+
+multiple = 0
+max_divide = max(divide)
+multiple += (n // max_divide) * (m // max_divide) * max_divide
+
+print(max_divide)
+print(multiple)
