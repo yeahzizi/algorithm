ba35c0936b4a088920c5bfcad3f89de4d002cb36
yeahzizi <newyeahzizi@gmail.com>
2023-08-17 01:01:49 +0900

    리트코드 dfs

---
+++ Leetcode/Medium/LC. 200. 섬의 개수.py
@@ -0,0 +1,24 @@
+#dfs 재귀 문제의 정석과도 같은 코드
+#공부하는데 도움이 많이 되었다
+#파이썬 알고리즘 인터뷰
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+
+            grid[i][j] = 0
+
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
