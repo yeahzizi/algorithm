e96f7a9b8b916be54d4f228dafa479f133225ef4
yeahzizi <newyeahzizi@gmail.com>
2023-08-24 16:51:23 +0900

    타겟넘버

--- 프로그래머스/코딩테스트 고득점 kit/level 2/PR. bfs. 타겟넘버.py
+++ 프로그래머스/코딩테스트 고득점 kit/level 2/PR. bfs. 타겟넘버.py
@@ -1,3 +1,19 @@
+# 여러 방법으로 풀어보기
+
 def solution(numbers, target):
+    n = len(numbers)
     answer = 0
+
+    def dfs(idx, value):
+        if idx == n:
+            if value == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx + 1, value + numbers[idx])
+            dfs(idx + 1, value - numbers[idx])
+
+    dfs(0, 0)
+
     return answer
