7ac39e91792f6bec436f3726057879317f0c31ae
yeahzizi <newyeahzizi@gmail.com>
2023-08-29 00:44:05 +0900

    230829 dp

--- 백준/코딩테스트 대비를 위한 백준 문제 추천 문제집/silver/BOJ. 15989. 1, 2, 3 더하기 4.py
+++ 백준/코딩테스트 대비를 위한 백준 문제 추천 문제집/silver/BOJ. 15989. 1, 2, 3 더하기 4.py
@@ -1,6 +1,15 @@
-#dp
+#dp 다시풀기...
 T = int(input())
-for t in range(T):
-    N = int(input())
+dp = [1] * 10001
 
-    
+for i in range(2, 10001):
+    dp[i] += dp[i - 2]
+
+for i in range(3, 10001):
+    dp[i] += dp[i - 3]
+
+for _ in range(T):
+    print(dp[int(input())])
+
+
+
