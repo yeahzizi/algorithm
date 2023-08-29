40d47d5ddc54ed2fad4dadc9dd0381fb5cc60be7
yeahzizi <newyeahzizi@gmail.com>
2023-08-29 23:30:20 +0900

    230829 dp 2차원배열

---
+++ 백준/코딩테스트 대비를 위한 백준 문제 추천 문제집/gold/BOJ. 12865. 평범한 배낭.py
@@ -0,0 +1,17 @@
+#dp
+#2차원 배열
+N, K = map(int, input().split())
+dp = [[0] * (K+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    w, v = map(int, input().split())
+    for j in range(1, K+1):
+        if w > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], v+dp[i-1][j-w])
+
+
+print(dp[N][K])
+
+
