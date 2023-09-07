315efd1e052bf52f89050bad2f2556402c4947db
yeahzizi <newyeahzizi@gmail.com>
2023-09-07 17:03:04 +0900

    230907 n-queen

--- 백준/gold/BOJ. 9663. N-queen.py
+++ 백준/gold/BOJ. 9663. N-queen.py
@@ -1,30 +1,28 @@
-def back_tracking():
-    global ans
-    if len(cnt) == N:
-        ans += 1
-        return
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            if chess[i][j] != 1:
-                for d in range(8):
-                    di = i + dx[d]
-                    dj = j + dx[d]
-                    if di >= 1 and di < N and dj >= 1 and dj < N:
-                        if chess[di][dj] == 1:
-                            break
-                        else:
-                            cnt.append(chess[i][j])
-                            back_tracking()
-                            cnt.pop()
-
-
-
-N = int(input())
-chess = [[1] * N for _ in range(1)] + [[1] * 1 + [0] * N for _ in range(N)]
-dx = [-1, -1, -1, 0, 1, 1, 1, 0]
-dy = [-1, 0, 1, 1, 1, 0, -1, -1]
-cnt = []
+import sys
+N = int(sys.stdin.readline().rstrip())
+rows = [0] * N #row[i] = j 라면 i행 j열에 퀸이 있다는 소리
 ans = 0
-back_tracking()
-print(ans)
 
+def is_safe(row):
+    # 같은 행 같은 열 같은 대각선에 체스가 있는지 체크
+    for i in range(row): # row인 이유 => 이전에 배치된 퀸들과의 충돌 확인 역할
+        if rows[i] == rows[row] or abs(row - i) == abs(rows[row] - rows[i]):
+            return False
+
+    return True
+
+
+def back_tracking(row):
+    global ans
+    if row == N:
+        ans += 1
+
+    else:
+        for col in range(N):
+            rows[row] = col
+            if is_safe(row):
+                back_tracking(row + 1)
+
+
+back_tracking(0)
+print(ans)
