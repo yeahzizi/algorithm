0391f3f778360446f7f4310c8815b678c64f9826
yeahzizi <newyeahzizi@gmail.com>
2023-09-22 00:03:05 +0900

    230922 구현

--- 백준/silver/BOJ. 1138. 한 줄로 서기.py
+++ 백준/silver/BOJ. 1138. 한 줄로 서기.py
@@ -1,6 +1,14 @@
 N = int(input())
 row = list(map(int, input().split()))
 height = [0] * N
-height[row[0]] = 1
 
-print(height)
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if cnt == row[i] and height[j] == 0:
+            height[j] = i+1
+            break
+        elif height[j] == 0:
+            cnt += 1
+
+print(*height)
