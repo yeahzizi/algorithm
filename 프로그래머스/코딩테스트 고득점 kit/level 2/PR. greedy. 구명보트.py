91ea869a008e6593b8a3a9df8c02ef353c29c13d
yeahzizi <newyeahzizi@gmail.com>
2023-10-14 02:29:26 +0900

    231014 그리디, 투포인터

---
+++ 프로그래머스/코딩테스트 고득점 kit/level 2/PR. greedy. 구명보트.py
@@ -0,0 +1,14 @@
+def solution(people, limit):
+    answer = 0
+
+    people.sort()
+    start = 0
+    end = len(people) - 1
+
+    while start < end:
+        if people[start] + people[end] <= limit:
+            start += 1
+            answer += 1
+        end -= 1
+
+    return len(people) - answer
