d7d0ea598ee0a55765f7b9117bf0873f53f4f2cf
yeahzizi <newyeahzizi@gmail.com>
2023-10-15 19:04:54 +0900

    231015 완전탐색

--- 프로그래머스/코딩테스트 고득점 kit/level 2/PR. 완전탐색. 카펫.py
+++ 프로그래머스/코딩테스트 고득점 kit/level 2/PR. 완전탐색. 카펫.py
@@ -12,3 +12,39 @@ def solution(brown, yellow):
                 answer.append(b)
 
     return answer
+
+# version 2 => 가 더 구림
+
+
+def solution(brown, yellow):
+    answer = []
+    whole = brown + yellow
+
+    a, b = 0, 0
+    for x in range(1, whole):
+        if whole % x == 0:
+            y = whole // x
+        if x >= y and x > 2 and y > 2 and (x - 2) * (y - 2) == yellow and x * y == whole:
+            a += x
+            b += y
+
+    answer.append(a)
+    answer.append(b)
+    return answer
+
+# version 3 => 거꾸로 해야 빠르다!
+def solution(brown, yellow):
+    answer = []
+    whole = brown + yellow
+
+    a, b = 0, 0
+    for x in reversed(range(whole)):
+        if whole % x == 0:
+            b = whole // x
+            a = x
+        if a >= b and a > 2 and b > 2 and (a - 2) * (b - 2) == yellow and a * b == whole:
+            break
+
+    answer.append(a)
+    answer.append(b)
+    return answer
