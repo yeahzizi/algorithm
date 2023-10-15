a35f39880b9ed3dc667584efcd9cb22ee946f58a
yeahzizi <newyeahzizi@gmail.com>
2023-10-15 18:11:11 +0900

    231015 dp

---
+++ 프로그래머스/level 2/PR. level2. 피보나치 수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+
+    if n >= 2:
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n] % 1234567
