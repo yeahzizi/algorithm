22c925d80e0ec098cb08c008941a771f96f7d0f7
yeahzizi <newyeahzizi@gmail.com>
2023-10-15 17:12:41 +0900

    231015 문자열

--- 프로그래머스/level 2/PR. level2. JadenCase 문자열 만들기.py
+++ 프로그래머스/level 2/PR. level2. JadenCase 문자열 만들기.py
@@ -8,3 +8,15 @@ def solution(s):
         else:
             answer += s[i]
     return answer
+
+# version 2
+def solution(s):
+    answer = ''
+    s = s.lower()
+    for i in range(len(s)):
+        if i == 0 or s[i - 1] == " ":
+            answer += s[i].upper()
+        else:
+            answer += s[i]
+
+    return answer
