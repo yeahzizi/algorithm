f147065cedd20d3f01089a80deeb000b53aadab9
yeahzizi <newyeahzizi@gmail.com>
2023-10-22 21:43:05 +0900

    231022 스택 2

--- 프로그래머스/코딩테스트 고득점 kit/level 2/PR. stackandqueue. 기능개발.py
+++ 프로그래머스/코딩테스트 고득점 kit/level 2/PR. stackandqueue. 기능개발.py
@@ -1,4 +1,15 @@
 # 진짜 간단한 실수에서 헤매다 ㅎㅎ
+# 테스트 1 〉	통과 (0.01ms, 10.1MB)
+# 테스트 2 〉	통과 (0.32ms, 10.1MB)
+# 테스트 3 〉	통과 (0.15ms, 10.1MB)
+# 테스트 4 〉	통과 (0.05ms, 10.2MB)
+# 테스트 5 〉	통과 (0.01ms, 10.3MB)
+# 테스트 6 〉	통과 (0.01ms, 10.2MB)
+# 테스트 7 〉	통과 (0.15ms, 10.2MB)
+# 테스트 8 〉	통과 (0.02ms, 10.1MB)
+# 테스트 9 〉	통과 (0.09ms, 10.2MB)
+# 테스트 10 〉	통과 (0.13ms, 10.2MB)
+# 테스트 11 〉	통과 (0.01ms, 10.1MB)
 import math
 
 
@@ -24,5 +35,43 @@ def solution(progresses, speeds):
             max_num = finish[i]
         else:
             cnt += 1
+
+    return answer
+
+# only stack  => 훨씬 빨라짐
+# 테스트 1 〉	통과 (0.01ms, 10.2MB)
+# 테스트 2 〉	통과 (0.04ms, 10.2MB)
+# 테스트 3 〉	통과 (0.05ms, 10.1MB)
+# 테스트 4 〉	통과 (0.03ms, 10.3MB)
+# 테스트 5 〉	통과 (0.01ms, 10.1MB)
+# 테스트 6 〉	통과 (0.01ms, 10.3MB)
+# 테스트 7 〉	통과 (0.05ms, 10.3MB)
+# 테스트 8 〉	통과 (0.01ms, 10.1MB)
+# 테스트 9 〉	통과 (0.02ms, 10.2MB)
+# 테스트 10 〉	통과 (0.03ms, 10.2MB)
+# 테스트 11 〉	통과 (0.01ms, 10.3MB)
+
+def solution(progresses, speeds):
+    answer = []
+    cnt = []
+    n = len(progresses)
+    for i in range(n):
+        now = 100 - progresses[i]
+        if now % speeds[i] == 0:
+            cnt.append(now // speeds[i])
+            continue
+        cnt.append((now // speeds[i]) + 1)
+
+    ans = 1
+    for i in range(len(cnt)):
+        if i == len(cnt) - 1:
+            answer.append(ans)
+            break
+        elif cnt[i] >= cnt[i + 1]:
+            cnt[i + 1] = cnt[i]
+            ans += 1
+        else:
+            answer.append(ans)
+            ans = 1
 
     return answer
