4d4afb25b1da78287c5844982b663c0a8b3427ea
yeahzizi <newyeahzizi@gmail.com>
2023-11-25 01:46:57 +0900

    231125 group by

---
+++ 프로그래머스/SQL 고득점 kit/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -0,0 +1,6 @@
+SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
+from FOOD_PRODUCT 
+where CATEGORY IN ('과자', '국', '김치', '식용유') 
+and price in (select max(price) from food_product group by category)
+group by CATEGORY
+order by PRICE desc
